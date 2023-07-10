7fb70c51db56301425db2a7a4ddafd88f042d774
r Star <starlove124760@gmail.com>
2023-07-10 22:45:10 +0800

    make Default_I_A non - accessible

--- Assets/Game/Default Interface/Scripts/MonoBehaviourForInterface.cs
+++ Assets/Game/Default Interface/Scripts/MonoBehaviourForInterface.cs
@@ -1,11 +1,20 @@
+#region
+
 using UnityEngine;
-using UnityEngine.EventSystems;
 using Zenject;
 
-public class MonoBehaviourForInterface : MonoBehaviour 
+#endregion
+
+public class MonoBehaviourForInterface : MonoBehaviour
 {
+#region Private Variables
+
     [Inject]
     private I_A a = I_A.Default;
+
+#endregion
+
+#region Unity events
 
     private void Start()
     {
@@ -17,39 +26,69 @@ public class MonoBehaviourForInterface : MonoBehaviour
         a.Log();
     }
 
+#endregion
+
+#region Private Methods
+
     private void Remove_A()
     {
         a = I_A.Default;
     }
+
+#endregion
 }
 
-class Class_A : I_A 
+class Class_A : I_A
 {
+#region Public Methods
+
     public void Log()
     {
         Debug.Log($"Class_A");
-    } 
+    }
+
+#endregion
 }
 
 internal interface I_A
 {
+#region Public Variables
+
+    static I_A Default => new Default_I_A();
+
+#endregion
+
+#region Private Variables
+
+    private class Default_I_A : I_A
+    {
+    #region Public Methods
+
+        public void Log() { }
+
+    #endregion
+    }
+
+#endregion
+
+#region Public Methods
+
     void Log()
     {
         Debug.Log($"I_A");
     }
-    static I_A Default => new Default_I_A();
 
-    internal class Default_I_A : I_A
-    {
-        public void Log() { }
-    }
+#endregion
 }
-
 
 class Class_B : I_A
 {
+#region Public Methods
+
     public void Log()
     {
         Debug.Log($"Class_B");
     }
+
+#endregion
 }
